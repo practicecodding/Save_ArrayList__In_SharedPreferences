14306d0b9180ceca457bbe50d78eaeceb9c409aa
user <practicecoddin@gmail.com>
2023-10-22 10:10:45 +0600

    Save ArrayList In SharedPreferences

---
+++ app/src/main/java/com/hamidul/savearraylistinsheredpreferences/MainActivity.java
@@ -0,0 +1,82 @@
+package com.hamidul.savearraylistinsheredpreferences;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    EditText edName,edAge;
+    Button button;
+    TextView textView;
+    ArrayList <ModelClass> arrayList;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        edName = findViewById(R.id.edName);
+        edAge = findViewById(R.id.edAge);
+        button = findViewById(R.id.button);
+        textView = findViewById(R.id.textView);
+
+        loadData();
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                saveData(edName.getText().toString(),edAge.getText().toString());
+            }
+        });
+
+
+    }
+
+    private void loadData() {
+
+        SharedPreferences sharedPreferences = getSharedPreferences(getString(R.string.app_name),MODE_PRIVATE);
+
+        Gson gson = new Gson();
+        String json = sharedPreferences.getString("product_data",null);
+        Type type = new TypeToken<ArrayList<ModelClass>>(){
+
+        }.getType();
+
+        arrayList = gson.fromJson(json,type);
+
+        if (arrayList == null){
+            arrayList = new ArrayList<>();
+            textView.setText(""+0);
+        } else {
+            for (int x=0; x<arrayList.size(); x++){
+                textView.setText(arrayList.get(x).name+"\n");
+            }
+        }
+
+    }
+
+    private void saveData(String name, String age) {
+
+        SharedPreferences sharedPreferences = getSharedPreferences(getString(R.string.app_name),MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+
+        Gson gson = new Gson();
+        arrayList.add(new ModelClass(name,Integer.parseInt(age)));
+        String json = gson.toJson(arrayList);
+        editor.putString("product_data",json);
+        editor.apply();
+        textView.setText("List Data\n");
+        loadData();
+
+    }
+}
---
+++ app/src/main/java/com/hamidul/savearraylistinsheredpreferences/ModelClass.java
@@ -0,0 +1,14 @@
+package com.hamidul.savearraylistinsheredpreferences;
+
+public class ModelClass {
+
+    public String name;
+    public int age;
+
+    public ModelClass(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+
+}
